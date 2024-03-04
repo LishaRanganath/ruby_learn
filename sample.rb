293771c22bd776c5911f58c719cd1a95bbb26992
Lisha Ranganath <87565510+LishaRanganath@users.noreply.github.com>
2024-03-05 01:03:47 +0530

    Add files via upload

---
+++ sample.rb
@@ -0,0 +1,7 @@
+# array=[1,2,3,4]
+# puts array.include?(2)
+Person=Struct.new(:name,:age,:gender)
+# p1=Person.new "john",28,"male"
+# p2=Person.new "david",30,"male"
+# puts [p1,p2].max_by(&:age)
+puts Person.new("peter")
